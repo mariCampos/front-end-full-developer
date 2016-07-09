1f5838632a2c7a295abb549fca39ad4b72b75257
Mariana Albuquerque <marianaalbuquerque02@gmail.com>
2016-07-09 17:20:24 -0300

    Adicionando conexao com o mongoDB

--- server.js
+++ server.js
@@ -12,19 +12,30 @@ app.listen(3000, function () {
 const express = require('express');
 const app = express();
 const path = require('path');
+const bodyParser = require('body-parser');
+const mongodb = require('mongodb');
+const mongoClient = mongodb.MongoClient;
 
 app.use(express.static(path.join(__dirname, 'public')));//Todos os arquivos nessa pasta serão visiveis
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine','ejs');
 
+app.use(bodyParser.urlencoded({extended: true})); //Vai permitir adicionar texto, imagens, GIF's
+
 app.get('/', (req, res) => {
   res.render('inicial');
 });
 
 app.post('/cadastrar',(req,res) => {
-	res.send('Cadastrou!');
-})
+	mongoClient.connect('mongodb://localhost:27017/jedi', function(err, db){
+		const usuarios = db.collection('usuarios');
+		usuarios.insertOne(req.body, function(err, result){
+			console.log(result.ops[0]);
+			res.send('Cadastrou!');
+		});
+	});	
+});
 
 app.listen(3000, () => {
   console.log('Example app listening on port 3000!');
